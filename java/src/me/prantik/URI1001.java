5c09a1e1be3c53f36e071efdb60759bcb666057d
Prantik <shahnoorprantik@gmail.com>
2021-07-24 16:09:48 +0600

    java | uri 1001 : accepted

---
+++ java/src/me/prantik/URI1001.java
@@ -0,0 +1,15 @@
+package me.prantik;
+// URI 1001 | Extremely Basic | BEGINNER | LEVEL 4 | + 4.2 POINTS
+
+import java.util.Scanner;
+
+public class URI1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("X = " + (a + b));
+    }
+}
---
+++ kotlin/src/main/kotlin/uri1001.kt
@@ -0,0 +1,8 @@
+// URI 1001 | Extremely Basic | BEGINNER | LEVEL 4 | + 4.2 POINTS
+
+fun main(args: Array<String>) {
+    val a: Int = readLine()?.toInt() ?: 0
+    val b: Int = readLine()?.toInt() ?: 0
+
+    println("X = ${a + b}")
+}
