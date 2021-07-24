814699f1fdb580bbe6edd3b7b04c73212acbf63b
Prantik <shahnoorprantik@gmail.com>
2021-07-24 14:52:04 +0600

    java | uri1000 : accepted

---
+++ java/src/me/prantik/URI1000.java
@@ -0,0 +1,8 @@
+package me.prantik;
+// URI 1044 | Hello World! | BEGINNER | LEVEL 2 | + 2.7 POINTS
+
+public class URI1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
