a3a5bbda62de32e8fb2a2ad24dcb06f2c61ae9f5
Prantik <shahnoorprantik@gmail.com>
2021-07-26 08:19:56 +0600

    java | uri 1003 : accepted

---
+++ java/src/me/prantik/URI1003.java
@@ -0,0 +1,15 @@
+package me.prantik;
+// URI 1003 | Simple Sum | BEGINNER | LEVEL 1 |  + 1.2 POINTS
+
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.printf("SOMA = %d\n", (a + b));
+    }
+}
---
+++ python/uri1002.py
@@ -0,0 +1,9 @@
+# URI 1002 | Area of a Circle | BEGINNER | LEVEL 4 |  + 4.9 POINTS
+
+R = float(input())
+
+a = 3.14159 * (R ** 2)
+
+a = str(('%.4f' % a))
+
+print('A=' + a)
