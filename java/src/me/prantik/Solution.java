9bdf04c025be1e89dd7a23ef8777bc0aa3b7f696
Prantik <coderprantik@gmai.com>
2021-08-17 07:46:57 +0600

    Hackerrank.com - Java Anagrams - soloved

--- java/src/me/prantik/Solution.java
+++ java/src/me/prantik/Solution.java
@@ -1,21 +1,34 @@
 package me.prantik;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        sc.close();
+    static int frequency(char c, String str) {
+        int count = 0;
 
-        System.out.println(isPalindrome(A) ? "Yes" : "No");
+        for (char _c : str.toCharArray()) {
+            if (Character.toString(c).equalsIgnoreCase(Character.toString(_c))) count++;
+        }
+        return count;
     }
 
-    public static boolean isPalindrome(String word) {
-        int s = 0, e = word.length();
-        while (word.charAt(s++) == word.charAt(--e) && s <= e);
-        return s > e;
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+
+        int pos = 0;
+        while (frequency(a.charAt(pos), a) == frequency(a.charAt(pos), b) && ++pos < a.length()) ;
+
+        return pos == a.length();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
     }
 }
-
